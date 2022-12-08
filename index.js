182fa241638df15fcf3d95a528078fff60462cf1
mojustmo-glitch <maureen.ngugi@student.moringaschool.com>
2022-12-08 22:53:33 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+//let newHeader = document.getElementById("victory");
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.textContent = "YOUR-NAME is the champion"
